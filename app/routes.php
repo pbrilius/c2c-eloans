99fce4adaa67d4fdd6e43c8d21fd533069fddb49
Povilas Brilius <pbrilius@gmail.com>
2021-07-31 16:28:18 +0300

    Slim framework application - initial commit.

---
+++ app/routes.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write(json_encode([
+          'superscript' => 'pbgroupeu',
+        ]));
+        return $response->withHeader('Content-type', 'application/json');
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
